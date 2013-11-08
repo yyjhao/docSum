013ccf611c5bd46bc296887491b11523c75701c6
yyjhao <yyjhao@gmail.com>
2013-11-09 00:18:07 +0800

    clean up

--- eval/eval.sh
+++ eval/eval.sh
@@ -1,4 +1,4 @@
-# ./ROUGE-1.5.5.pl -e data -n 4 -z SPL paper.spl.lst
+./ROUGE-1.5.5.pl -d -U -s -e data -n 4 -z SPL paper.spl.lst
 # ./ROUGE-1.5.5.pl -e data -n 4 -z SPL papers_coref.lst
 # ./ROUGE-1.5.5.pl -e data -n 4 -z SPL papers_pageRank.lst
 # ./ROUGE-1.5.5.pl -e data -n 4 -z SPL papers_overlap.lst
